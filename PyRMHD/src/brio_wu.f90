2d1e7f12b2f1ee0adbd4d5ad4d65eac767c85b11
~/.gitconfig <esquivel@nemesis.nucleares.unam.mx>
2014-06-10 12:55:15 -0500

    Brio-Wu shock tube test
    This module is loaded by the user defined module (user_mod)

--- PyRMHD/src/brio_wu.f90
+++ PyRMHD/src/brio_wu.f90
@@ -36,45 +36,37 @@ contains
   end subroutine init_bw
 
   !--------------------------------------------------------------------
-
-  subroutine impose_bw(u,time)
-    use globals, only : coords, dx, dy, dz, rank
+  ! Initial conditions for the BRIO-WU shock tube
+  subroutine impose_bw(u)
+    use globals, only : coords, dx
     use parameters, only : nxtot
     implicit none
     real, intent(out) :: u(neq,nxmin:nxmax,nymin:nymax,nzmin:nzmax)
-    real, intent (in)   :: time
-    real :: x !, y, z, xpl, ypl, zpl
-! BRIO-WU 
-    real :: rhoL, rhoR, vxL, vxR, vyL, vyR, vzL, vzR, pL, pR
-    real :: BxL, BxR, ByL, ByR, BzL, BzR
-
+    real :: x
     integer ::  i,j,k
 
     do i=nxmin,nxmax
        do j=nymin,nymax
           do k=nzmin,nzmax
 
-             ! Position measured from the centre of the grid (star)
+             ! Position measured from the centre of the grid
              x=(float(i+coords(0)*nx-nxtot/2)+0.5)*dx
 
              ! IF LEFFT STATE
-             if( x <= nxtot/2) then
+             if( x <= 0.) then
 
                 !   total density and momena
                 u(1,i,j,k) = rhoL
                 u(2,i,j,k) = rhoL*vxL
                 u(3,i,j,k) = rhoL*vyL
                 u(4,i,j,k) = rhoL*vzL
-                !   energy
-                u(5,i,j,k)=0.5*rhoL*(vxL**2+vyL**2+vzL**2)+cv*pL
-#ifdef MHD
-                !  add magnetic energy
-                u(5,i,j,k) =  u(5,i,j,k)+0.5*(BxL**2+ByL**2+BzL**2)
-
+                !   total energy
+                u(5,i,j,k)=0.5*rhoL*(vxL**2+vyL**2+vzL**2)+cv*pL  &
+                          +0.5*(BxL**2+ByL**2+BzL**2)
                 u(6,i,j,k) =  BxL
                 u(7,i,j,k) =  ByL
                 u(8,i,j,k) =  BZL
-#endif
+
              else !RIGHT STATE
                 !   total density and momena
                 u(1,i,j,k) = rhoR
@@ -82,17 +74,12 @@ contains
                 u(3,i,j,k) = rhoR*vyR
                 u(4,i,j,k) = rhoR*vzR
                 !   energy
-                u(5,i,j,k)=0.5*rhoR*(vxR**2+vyR**2+vzR**2)+cv*pR
-
-#ifdef MHD
-                !  add magnetic energy
-                u(5,i,j,k) =  u(5,i,j,k)+0.5*(BxR**2+ByR**2+BzR**2)
-
+                u(5,i,j,k)=0.5*rhoR*(vxR**2+vyR**2+vzR**2)+cv*pR   &
+                          +0.5*(BxR**2+ByR**2+BzR**2)
                 u(6,i,j,k) =  BxR
                 u(7,i,j,k) =  ByR
                 u(8,i,j,k) =  BZR
 
-#endif
              end if
                 
           end do
